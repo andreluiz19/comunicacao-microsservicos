3bbf4c39e1c7ec14f8b9d10c5347b677d4ae29d8
André Avancini <andreavancini@alunos.utfpr.edu.br>
2023-06-27 21:50:37 -0300

    auth-api finished

---
+++ auth-api/src/config/db/initialData.js
@@ -0,0 +1,21 @@
+import bcrypt from "bcrypt";
+
+import User from "../../modules/user/model/User.js";
+
+export async function createInitialData() {
+    await User.sync({ force: true });
+
+    let password = await bcrypt.hash("123456", 10);
+
+    await User.create({
+        name: "User Test 1",
+        email: "testuser1@gmail.com",
+        password: password,
+    });
+
+    await User.create({
+        name: "User Test 2",
+        email: "testuser2@gmail.com",
+        password: password,
+    });
+}
---
+++ auth-api/src/modules/user/controller/UserController.js
@@ -0,0 +1,15 @@
+import UserService from "../service/UserService.js";
+
+class UserController {
+    async findByEmail(req, res) {
+        let user = await UserService.findByEmail(req);
+        return res.status(user.status).json(user);
+    }
+
+    async getAccessToken(req, res) {
+        let accessToken = await UserService.getAccessToken(req);
+        return res.status(accessToken.status).json(accessToken);
+    }
+}
+
+export default new UserController();
